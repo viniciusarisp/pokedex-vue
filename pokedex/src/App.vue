4155eb06ced077ee9d63b44527aba926266b5663
viniciusarisp <viniciusarisp@gmail.com>
2023-01-08 13:36:31 -0300

    recebendo a lista de todos pokemons

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-card>
+            {{ pokemons }}
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
+  components: {},
+
+  data() {
+    return {
+      pokemons: [],
+    }
   },
 
-  data: () => ({
-    //
-  }),
+  mounted(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0").then((response) => {
+      this.pokemons = response.data.results
+    })
+  }
 }
 </script>
