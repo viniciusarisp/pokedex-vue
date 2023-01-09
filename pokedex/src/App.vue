9b88af5ffe10438b2d97615eef37f63353de97a0
viniciusarisp <viniciusarisp@gmail.com>
2023-01-09 10:26:00 -0300

    construindo a card pokemon

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -45,8 +45,9 @@ export default {
       return b.id - a.id;
       });
     },
-    show_pokemon() {
-      this.showCard = !this.showCard
+    show_pokemon(pokemon) {
+      this.showCard = !this.showCard;
+      this.selectedPokemon = pokemon;
     }
   },
 
@@ -57,8 +58,10 @@ export default {
   computed: {
     // Quando a busca muda, recalcula-se o filtro e retorna pokesfiltrados()
     pokesFiltrados() {
+      const search = this.busca.toLowerCase();
       return this.pokemons.filter((item) => {
-        return item.name.includes(this.busca)
+        const name = item.name.toLowerCase();
+        return name.includes(search);
       }) 
     }
   }
@@ -101,13 +104,46 @@ export default {
       v-model="showCard"
       width="500"
     >
-      <v-card>
-        <v-container></v-container>
-       </v-card>
+          <v-row>
+            <v-col cols="12">
+              <v-card class="pokemon-card">
+                <img :src="selectedPokemon.sprite" :alt="selectedPokemon.name"/>
+                <h2 class="text-center text-capitalize"> {{ selectedPokemon.name }}</h2>
+                <p class="text-center">Tipos: {{ selectedPokemon.types.join(', ') }}</p>
+                <p class="text-center">Habilidades: {{ selectedPokemon.abilities.join(', ') }}</p>
+                <p class="text-center">Peso: {{ selectedPokemon.weight }}</p>
+                <p class="text-center">Altura: {{ selectedPokemon.height }}</p>
+              </v-card>
+            </v-col>
+          </v-row>
     </v-dialog>
   </v-app>
 </template>
 
 <style>
+  .pokemon-card {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    background-color: #f8f8f8;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    font-family: sans-serif;
+  }
+  .pokemon-card img {
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 5px;
+  }
+  .pokemon-card h2 {
+    font-size: 24px;
+    color: #333;
+    margin: 10px 0;
+    text-transform: uppercase;
+  }
+  .pokemon-card p {
+    font-size: 16px;
+    color: #666;
+    margin: 10px 0;
+  }
 </style>
-
