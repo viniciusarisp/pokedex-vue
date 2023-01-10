00a14ec38e5473406c876985b38c711eaebbe534
viniciusarisp <viniciusarisp@gmail.com>
2023-01-10 14:26:33 -0300

    estilizando header/footer

--- pokedex/src/components/footer.vue
+++ pokedex/src/components/footer.vue
@@ -23,10 +23,13 @@ export default {
 
 </script>
 <template>
-      <v-footer class="bg-red-darken-4 text-center d-flex flex-column h-auto">
+      <v-footer class="bg-red-darken-4 text-center d-flex flex-column h-auto elevation-8 rounded-t-xl">
             <div>
                   <v-btn v-for="icon in icons" :key="icon" class="mx-4" :icon="icon.iconsrc" variant="text" :href="icon.socialsrc" target="_blank"></v-btn>
             </div>
+            <p>
+                  Pokedex created with vue 3 and vuetify.
+            </p>
             <div>
                   {{ new Date().getFullYear() }} — <strong>Vinicius Aristimunha Pinto</strong>
             </div>
--- pokedex/src/components/header.vue
+++ pokedex/src/components/header.vue
@@ -8,8 +8,8 @@ export default {
 
 </script>
 <template>
-      <v-header class="bg-red-darken-4 text-center d-flex flex-column">
-                  <v-img  :aspect-ratio="16/9" style="width: 35vw;" class="img-header my-5 mx-auto" src="https://user-images.githubusercontent.com/29473781/180619084-a56960ab-7efa-4e34-9d33-4e3e581d62ff.png" alt="pokedex logo" />
+      <v-header class="bg-red-darken-4 text-center d-flex flex-column elevation-8 rounded-b-xl">
+                  <v-img  :aspect-ratio="16/9" class="img-header my-5 mx-auto w-25" src="https://user-images.githubusercontent.com/29473781/180619084-a56960ab-7efa-4e34-9d33-4e3e581d62ff.png" alt="pokedex logo" />
       </v-header>
 </template>
 <style>
