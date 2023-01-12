5dd5a680f50d7142c34654a716c6041d011d395b
viniciusarisp <viniciusarisp@gmail.com>
2023-01-12 19:00:32 -0300

    modularizando estilos globais de css e limpando código

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default {
     }
   },
   methods: {
-    //trata o pokemon recebido por MainList.vue
+    //trata o Pokémon recebido por MainList.vue
     setPokemon(pokemon) {
       this.selectedPokemon = pokemon;
       this.showCard = !this.showCard;
@@ -40,7 +40,7 @@ export default {
     }
   },
     mounted() {
-      // Faz uma requisição à API de pokémons ao montar o componente
+      // Faz uma requisição à API de Pokémons ao montar o componente
       const url = "https://pokeapi.co/api/v2/pokemon?limit=32&offset=0";
       this.getList(url);
     },
@@ -50,84 +50,11 @@ export default {
 <template>
   <v-app>
     <MainHeader />
-    <!-- Mainlist devolve o pokemon selecionado para app.vue -->
+    <!-- Mainlist devolve o Pokémon selecionado para app.vue -->
     <MainList :pokemons="pokemons" @selectPokemon="setPokemon($event)"/>
-    <!-- PopUpCard Recebe o pokemon selecionado após ser definido na função setPokemon -->
+    <!-- PopUpCard Recebe o Pokémon selecionado após ser definido na função setPokemon -->
     <PopUpCard :selectedPokemon="selectedPokemon" v-model="showCard" />
     <MainFooter />
   </v-app>
 </template>
 
-<style>
-.grass {
-  background-color: #8bd8517e;
-}
-
-.poison {
-  background-color: #aa5ca083;
-}
-
-.fire {
-  background-color: #fa544286;
-}
-
-.flying {
-  background-color: #78a3ff79;
-}
-
-.water {
-  background-color: #57acfb71;
-}
-
-.bug {
-  background-color: #c3d2218e;
-}
-
-.normal {
-  background-color: #babbad86;
-}
-
-.electric {
-  background-color: #fbe13885;
-}
-
-.ground {
-  background-color: #edc75583;
-}
-
-.fairy {
-  background-color: #faafff81;
-}
-
-.fighting {
-  background-color: #ad55417c;
-}
-
-.psychic {
-  background-color: #f561b07c;
-}
-
-.rock {
-  background-color: #cebc7275;
-}
-
-.ghost {
-  background-color: #7671d070;
-}
-
-.ice {
-  background-color: #95f2fe77;
-}
-
-.dragon {
-  background-color: #8474ff73;
-}
-
-.dark {
-  background-color: #8c67547c;
-}
-
-.steel {
-  background-color: #c4c2da7a;
-}
-</style>
